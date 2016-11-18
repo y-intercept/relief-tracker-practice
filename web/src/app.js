fdc91e8ffc5e524d004f5d2eccca4261d8e1f75a
Joseph <jcdaniel55@gmail.com>
2016-11-18 11:49:00 -0500

    step 3 persons - componentDidMoutnt - xhr - Miss

--- web/src/app.js
+++ web/src/app.js
@@ -1,7 +1,15 @@
 const React = require('react')
-const { BrowserRouter, Match } = require('react-router')
+const { BrowserRouter, Match, Miss, Link } = require('react-router')
 const Home = require('./pages/home')
 const About = require('./pages/about')
+const Persons = require('./pages/persons')
+
+const NoMatch = () => (
+	<div>
+		<h3>Page Not Found</h3>
+		<Link to="/">Home</Link>
+	</div>
+)
 
 const App = React.createClass({
 	render() {
@@ -10,6 +18,8 @@ const App = React.createClass({
 				<div>
 					<Match exactly pattern="/" component={Home} />
 					<Match exactly pattern="/about" component={About} />
+					<Match exactly pattern="/persons" component={Persons} />
+					<Miss component={NoMatch} />
 				</div>
 		 </BrowserRouter>
 		)
---
+++ web/src/pages/persons/index.js
@@ -0,0 +1,35 @@
+const React = require('react')
+const { Link } = require('react-router')
+const xhr = require('xhr')
+
+
+const Persons = React.createClass({
+	getInitialState() {
+		return {
+			persons: []
+		}
+	},
+	componentDidMount() {
+		xhr.get('http://localhost:4000/persons', {
+			json: true
+		}, (err, res, persons) => {
+			if (err) return console.log(err.message)
+			this.setState({persons})
+		})
+	},
+	render() {
+		const listPerson = person =>
+			<li>{person.firstName + ' ' + person.lastName}</li>
+		return (
+			<div>
+				<h2>TODO: Persons List</h2>
+				<ul>
+					{this.state.persons.map(listPerson)}
+				</ul>
+				<Link to="/">Home</Link>
+			</div>
+		)
+	}
+})
+
+module.exports = Persons
